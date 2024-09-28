1cad80a7c5b3273a0d7504707aa61a008e809467
NoahSantos <noahbsantos@gmail.com>
2024-09-28 15:29:34 -0700

    Context
    Context is created for the signup page

--- client/src/app/dashboard/page.jsx
+++ client/src/app/dashboard/page.jsx
@@ -1,8 +1,16 @@
 "use client";
 
 import React from 'react'
+import {useEffect, useContext} from 'react'
+import {UserContext} from '../layout';
 
 const Dashboard = () => {
+    const {name, userName, password, email, skills:userSkills, pastWorks, credits, averageRating, update} = useContext(UserContext);
+
+    useEffect(() => {
+        console.log({name, userName, password, email, userSkills, pastWorks, credits, averageRating});
+    },)
+    
     return (
         <div>Welcome to the Dashboard</div>
     )
