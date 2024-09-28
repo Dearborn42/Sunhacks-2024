8b1d0367cad0ba22f240850086b469a6e6f55909
Dearborn42 <amurf26@outlook.com>
2024-09-28 15:30:47 -0700

    working on server functions

--- client/src/middleware.js
+++ client/src/middleware.js
@@ -2,8 +2,18 @@ import { NextResponse } from "next/server";
 
 export function middleware(req){
   if(req.nextUrl.pathname === "/") return NextResponse.next();
-  
-  // Implement checks here, currently middleware will check every path including the root path.
+  if (
+    req.nextUrl.pathname.startsWith('/_next') || 
+    req.nextUrl.pathname.startsWith('/favicon.ico') || 
+    req.nextUrl.pathname.startsWith("/api") || 
+    req.nextUrl.pathname.startsWith('/images') ||
+    req.nextUrl.pathname === "/signup" || 
+    req.nextUrl.pathname === "/login"
+  ) return NextResponse.next();
+
+  if(!req.cookies.has("access")){
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
   return NextResponse.next();
 }
