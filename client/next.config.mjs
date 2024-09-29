5ea9c72f5afe76986143059d72e9840971e83ebe
NoahSantos <noahbsantos@gmail.com>
2024-09-28 20:41:18 -0700

    Login
    Styled the login page

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinifyMode: true,
+    images: {
+        remotePatterns: [
+        {hostname: 'res.cloudinary.com'},{hostname: 'cdn.pixabay.com'},{hostname: 'encrypted-tbn0.gstatic.com'}, {hostname: 'images.immediate.co.uk'}, {hostname: 'images.squarespace-cdn.com'}, {hostname: 'arc.stimg.co'},
+        ],
+        domains: ['lh3.googleusercontent.com']
+    }
+};
 
 export default nextConfig;
--- client/src/app/login/page.jsx
+++ client/src/app/login/page.jsx
@@ -3,7 +3,12 @@ import * as React from "react";
 import { useState, useContext } from "react";
 import { UserContext } from "../layout";
 import { login } from '@/Components/ServerFunctions';
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
+import Link from "next/link";
+import { MdOutlineMail } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
+import Image from 'next/image';
+import img from '../../public/image.png'
 
 export default function LoginPage(){
     const { update } = useContext(UserContext);
@@ -22,30 +27,56 @@ export default function LoginPage(){
         });
     }
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col items-center">
-            <input
-                className="text-black" 
-                type="email"
-                autoComplete="true"
-                name="email"
-                id="email"
-                placeholder="Enter your email address"
-                value={form.email}
-                required
-                onChange={(e) => handleForm("email", e.target.value)}
-            />
-            <input
-                className="text-black"
-                type="password"
-                autoComplete="true"
-                name="password"
-                id="password"
-                placeholder="Enter your password"
-                value={form.password}
-                required
-                onChange={(e) => handleForm("password", e.target.value)}
-            />
-            <button type="submit">Login</button>
-        </form>
+        <div className="loginCont">
+            <form onSubmit={handleSubmit} className="loginForm">
+                <div className="loginInfo">
+                    <h1 className="title">Login</h1>
+                    <div className="inputCont">
+                        <label htmlFor="email">Email</label>
+                        <div>
+                            <MdOutlineMail className="icon"/>
+                            <input
+                                className="text-black" 
+                                type="email"
+                                autoComplete="true"
+                                name="email"
+                                id="email"
+                                placeholder="example@gmail.com"
+                                value={form.email}
+                                required
+                                onChange={(e) => handleForm("email", e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div className="inputCont">
+                        <label htmlFor="password">Password</label>
+                        <div>
+                            <RiLockPasswordLine className="icon"/>
+                            <input
+                                className="text-black"
+                                type="password"
+                                autoComplete="true"
+                                name="password"
+                                id="password"
+                                placeholder="Enter your password"
+                                value={form.password}
+                                required
+                                onChange={(e) => handleForm("password", e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <p>Don't have an account yet? <Link href="../signup" className="signup">Sign Up</Link></p>
+                    <button type="submit">Login</button>
+                </div>
+                <div className="imgCont">
+                    <Image
+                    src={img}
+                    width={500}
+                    height={500}
+                    alt="login"
+                    />
+                </div>
+            </form>
+        </div>
     )
 }
